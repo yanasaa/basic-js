1431fef3d660149b36d8ecacf53a9cbc87117fe6
yanasaa <yanasaa@gmail.com>
2025-11-10 00:51:11 +0300

    add VigenereCipheringMachine funcs

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,54 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor(direct = true) {
+    this.direct = direct;
   }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    return this._process(message, key, 'encrypt');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    return this._process(encryptedMessage, key, 'decrypt');
+  }
+
+  _process(text, key, mode) {
+    const result = [];
+    let keyIndex = 0;
+    const upperText = text.toUpperCase();
+    const upperKey = key.toUpperCase();
+
+    for (let i = 0; i < upperText.length; i++) {
+      const char = upperText[i];
+      
+      if (char >= 'A' && char <= 'Z') {
+        const textCode = char.charCodeAt(0) - 65;
+        const keyCode = upperKey[keyIndex % upperKey.length].charCodeAt(0) - 65;
+        
+        let processedCode;
+        if (mode === 'encrypt') {
+          processedCode = (textCode + keyCode) % 26;
+        } else {
+          processedCode = (textCode - keyCode + 26) % 26;
+        }
+        
+        result.push(String.fromCharCode(processedCode + 65));
+        keyIndex++;
+      } else {
+        result.push(char);
+      }
+    }
+
+    return this.direct ? result.join('') : result.reverse().join('');
   }
 }
 
